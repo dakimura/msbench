8554062de83d6017f55813b03accc96cafcf32d2
Daito AKIMURA <dakimura@mercari.com>
2020-02-25 18:26:29 +0900

    chore: release version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="msbench",
-    version="1.0",
+    version="1.0.1",
     description="Benchmarking tool for marketstore",
     author="Daito Akimura",
     install_requires=["pymarketstore", "argparse", ],
